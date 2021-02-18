aea89958d684bf4accfe822945e93eec76ca24aa
isgleb <71262423+isgleb@users.noreply.github.com>
2021-02-18 21:22:15 +0300

    form controller upgraded

--- src/main/java/org/example/FormController.java
+++ src/main/java/org/example/FormController.java
@@ -2,10 +2,26 @@ package org.example;
 
 import java.io.IOException;
 import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 
 public class FormController {
 
     private Long orderId;
+
+    private PaymentDetails paymentDetails;
+
+    @FXML private TextField clientId;
+    @FXML private TextField ownersName;
+    @FXML private TextField address;
+    @FXML private TextField sum;
+
+    @FXML private TextField coldWater;
+    @FXML private TextField hotWater;
+    @FXML private TextField electricity;
+    @FXML private TextField repairment;
+
+
+
 
     @FXML
     private void switchToPrimary() throws IOException {
@@ -23,4 +39,20 @@ public class FormController {
 
         System.out.println(orderId);
     }
+
+    @FXML
+    private void saveChanges() {
+
+        clientId.setText("0");
+        ownersName.setText("0");
+        address.setText("0");
+        sum.setText("0");
+
+        coldWater.setText("0");
+        hotWater.setText("0");
+        electricity.setText("0");
+        repairment.setText("no");
+
+    }
+
 }
---
+++ src/main/java/org/example/PaymentDetails.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.Date;
+
+public class PaymentDetails {
+
+    Long clientId;
+    String ownersName;
+    String address;
+    Date period;
+    int summ;
+
+    int coldWater;
+    int hotWater;
+    int electricity;
+    int repairment;
+
+}
